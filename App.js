2da7c5a175bf5ff4ba11696cbb22d047ddd7f8c6
Tiffany W <t.washington0008@gmail.com>
2022-12-21 21:09:26 -0800

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+// import { store } from './redux/store';
+//the next 3 imported for persist in combo with what you did on store.js
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 export default function App() {
   // return <Main />;
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        {/* must wrap around all components that use navigation in our app (like Main where all our nav setup will reside)  */}
-        <Main />
-      </NavigationContainer>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+      {/* PersistGate component will help integrate it w/ React and REact Native apps, prevents from loading until the Redux store has been rehydrated fully from the client-side storage, whatever you pass is what it will show while loading- so we used our Loading component */}
+        <NavigationContainer>
+          {/* must wrap around all components that use navigation in our app (like Main where all our nav setup will reside)  */}
+          <Main />
+        </NavigationContainer>
+        </PersistGate>
     </Provider>  
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -4,13 +4,59 @@ import { commentsReducer } from '../features/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { favoritesReducer } from '../features/favorites/favoritesSlice';
+//importing the persistStore so can save some things (ex. Favorites) on client side storage (so accessible offline!! and on the emulator you can close it and open and the favorites are still there) and can also use other storage
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+
+
+//****** added "const config" with presist */
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+}
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer,
         favorites: favoritesReducer
-    }
+    }),
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [
+                FLUSH,
+                REHYDRATE,
+                PAUSE,
+                PERSIST,
+                PURGE,
+                REGISTER
+            ]
+        }
+    })
 });
+export const persistor = persistStore(store);
+
+//**** replaced below with above with Persist */
+// export const store = configureStore({
+//     reducer: {
+//         campsites: campsitesReducer,
+//         comments: commentsReducer,
+//         partners: partnersReducer,
+//         promotions: promotionsReducer,
+//         favorites: favoritesReducer
+//     }
+// });
