fa6f747f1b9c8071ba163bf3059ffc651a4b0d86
Tiffany W <t.washington0008@gmail.com>
2022-12-21 20:38:35 -0800

    Gestures Part 2

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,11 +1,16 @@
 //adding PanResponder and Alert for the response to the slide towards the right and the alert to see if you want to add the campsite as a favorite
 import { Text, View, StyleSheet, PanResponder, Alert } from 'react-native';
+//Gesture 2 (don't like too much bt can use this elsewhere versus how used in this app to bounce if make a gesture on capsite directory, rendered campsite-- below will import useRef from React so can give the user a heads up when they have started a gesture
+import { useRef } from 'react';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    //Gesture 2- create a reference to it, Ref is similar concept-wise to how you use an id in html element to use later in your JS code to getElementById
+    const view  = useRef();
 
     const isLeftSwipe = ({ dx }) => dx < -200;
     //for the isLeftSwipe fx's parameter, it will take an object and destructure from it dx is like delta x or the distance of a gesture across the x axis
@@ -14,11 +19,24 @@ const RenderCampsite = (props) => {
     //create a pan responder and keep it in a constant name, here we name it the same:: under the Animatable View you will connect it to the component
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        
+        //Gesture 2 to show the user they are triggering a gesture; below is triggered when a gesture is first recognized
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                //below, the animation will return a promise, that will resolve at the end of the animation that will contain the property as finished
+                .then((endState)=> console.log(endState.finished ? 'finished' : 'canceled')
+                ); 
+        }, 
+        //below was Gesture 1- keep this
         onPanResponderEnd: (e, gestureState) => {
         //above is an event handler; the evenet we don't need as much so just putting because need it to be able to get to the 2nd parameter- gesture state
-            console.log('pan responder end', gestureState);
+            
+       
+        
+        console.log('pan responder end', gestureState);
             if (isLeftSwipe(gestureState)) {
-                Alert.alert(
+                    Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' + campsite.name + ' to favorites?',
                     [
@@ -51,6 +69,10 @@ const RenderCampsite = (props) => {
             //below we are connecting the PanResponder to a component
             {...panResponder.panHandlers}
             // above spreads the panResponders pan handlers to recombine as 1 obj to pass in as props to this component
+            
+            //Gesture 2 add- ny React components that are defined as class components, like Animatable.View is, can use a useRef prop
+            ref={view}
+            //{view} is what we createed above with const view, now add a Pand Responder-- onPanResponderGrant further below
         >
             <Card containerStyle={styles.cardContainer}>
                 {/* <Card.Image source= {campsite.image}> */}
