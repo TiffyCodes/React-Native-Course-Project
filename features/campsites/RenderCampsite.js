552b903f1b7c0e2d7cd6b3c9884402c27a48edf5
Tiffany W <t.washington0008@gmail.com>
2022-12-21 20:16:27 -0800

    Gestures Part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,5 @@
-import { Text, View, StyleSheet } from 'react-native';
+//adding PanResponder and Alert for the response to the slide towards the right and the alert to see if you want to add the campsite as a favorite
+import { Text, View, StyleSheet, PanResponder, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
@@ -6,8 +7,38 @@ import * as Animatable from 'react-native-animatable';
 const RenderCampsite = (props) => {
     const { campsite } = props;
 
-    
+    const isLeftSwipe = ({ dx }) => dx < -200;
+    //for the isLeftSwipe fx's parameter, it will take an object and destructure from it dx is like delta x or the distance of a gesture across the x axis
+    //so we will recognize a gesture where there is a horiz drag to the left that is smaller than -200px; so -300 would be smaller and -100 bigger 
 
+    //create a pan responder and keep it in a constant name, here we name it the same:: under the Animatable View you will connect it to the component
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+        //above is an event handler; the evenet we don't need as much so just putting because need it to be able to get to the 2nd parameter- gesture state
+            console.log('pan responder end', gestureState);
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.isFavorite
+                            ? console.log('Already set as a favorite.')
+                            : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+        }
+    });
     
 
     //below is conditional rendering
@@ -17,6 +48,9 @@ const RenderCampsite = (props) => {
             animation = 'fadeInDownBig'
             duration={2000}
             delay={1000}
+            //below we are connecting the PanResponder to a component
+            {...panResponder.panHandlers}
+            // above spreads the panResponders pan handlers to recombine as 1 obj to pass in as props to this component
         >
             <Card containerStyle={styles.cardContainer}>
                 {/* <Card.Image source= {campsite.image}> */}
